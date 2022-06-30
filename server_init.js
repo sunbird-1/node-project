77191c81f6776bfa25862d76c1cdf34895a2412a
Ritu1997 <rituparnamajhi333j@gmail.com>
2022-06-30 05:11:45 +0000

    pushed in git repo

---
+++ server_init.js
@@ -0,0 +1,14 @@
+// Load express module using `require` directive
+let express = require('express')
+let app = express()
+
+// Define request response in root URL (/)
+app.get('/', function (req, res) {
+res.send('Dockerize the node app')
+})
+
+// Launch listening server on port 8081
+app.listen(8081, function () {
+console.log('app listening on port 8081')
+})
+
