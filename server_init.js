6e5514d05fbb95f25d8c78b3c6c96d7751814e21
rituparna1997 <80688786+rituparna1997@users.noreply.github.com>
2022-06-30 19:19:33 +0530

    Update server_init.js

--- server_init.js
+++ server_init.js
@@ -4,7 +4,7 @@ let app = express()
 
 // Define request response in root URL (/)
 app.get('/', function (req, res) {
-res.send('<center><h1>Welcome to my docorized node js app</h1></center>')
+res.send('<center><h1>Hay!! Welcome to my docorized node js app</h1></center>')
 })
 
 // Launch listening server on port 8081
