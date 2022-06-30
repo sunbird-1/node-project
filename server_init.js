872fd68d8e9a5eab5a0947a978bae5c15c349050
rituparna1997 <rituparnamajhi333j@gmail.com>
2022-06-30 07:00:57 +0000

    test build trigger

--- server_init.js
+++ server_init.js
@@ -4,7 +4,7 @@ let app = express()
 
 // Define request response in root URL (/)
 app.get('/', function (req, res) {
-res.send('Dockerize the node app')
+res.send('<center><h1>Dockerize the node app</h1></center>')
 })
 
 // Launch listening server on port 8081
