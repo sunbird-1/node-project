34c86aeb652d6eb0d56b76052026be646011fe40
rituparna1997 <80688786+rituparna1997@users.noreply.github.com>
2022-07-04 23:47:45 +0530

    Update server_init.js

--- server_init.js
+++ server_init.js
@@ -4,7 +4,7 @@ let app = express()
 
 // Define request response in root URL (/)
 app.get('/', function (req, res) {
-res.send('<center><h1>Hello, world!!! I am rituparna</h1></center>')
+res.send('<center><h1>Hello, world!!! I am rituparna majhi</h1></center>')
 })
 
 // Launch listening server on port 8081
